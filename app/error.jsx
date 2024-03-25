78c94dd76e4490bc75efb2633e97081d8f2415a6
Jado0o0o <76520626+ailaa-jadoo@users.noreply.github.com>
2024-03-25 22:12:43 +0530

    all main features added

--- app/error.jsx
+++ app/error.jsx
@@ -2,6 +2,9 @@
  
 import { useEffect, useState } from 'react'
 import { useRouter } from 'next/navigation'
+import {Card, CardFooter, CardBody, Image, CardHeader, Divider, Button, Code} from "@nextui-org/react";
+import skelly from '../public/skeleton-svgrepo-com.png'
+
 export default function Error({ error, reset }) {
   const router = useRouter()
   const [secondsRemaining, setSecondsRemaining] = useState(10);
@@ -22,9 +25,31 @@ export default function Error({ error, reset }) {
   }, [error, router, secondsRemaining]);
 
   return (
-    <div>
-      <h2>Something went wrong!</h2>
-      <p>Redirecting to home page in {secondsRemaining} seconds...</p>
+    <div className='flex justify-center items-center h-screen px-2'>
+      <Card isFooterBlurred
+      radius="lg" className="max-w-[400px] md:max-w-2xl border-none">
+      <CardHeader className="flex gap-3">
+        <Image
+          alt="skelly logo"
+          height={40}
+          radius="sm"
+          src="https://i.ibb.co/Q7gjd3N/skeleton-svgrepo-com.png"
+          width={40}
+        />
+        <div className="flex flex-col">
+          <p className="text-lg text-danger font-bold">Error</p>
+          <p className="text-md">Go back to <span className='text-warning font-bold'>nitdelhi.vercel.app</span></p>
+        </div>
+      </CardHeader>
+      <Divider/>
+      <CardBody>
+        <p>Make sure the Entered <span className='font-bold c400'>Roll No.</span> is similar to <Code className='c400'>201230012</Code> and of length 9</p>
+      </CardBody>
+      <Divider/>
+      <CardFooter>
+        Redirecting to home page in {secondsRemaining} seconds ...
+      </CardFooter>
+    </Card>
     </div>
   )
 }
--- app/profile/profile.jsx
+++ app/profile/profile.jsx
@@ -35,14 +35,14 @@ export default function Profile() {
             }
             const data = await response.text();
             setStudentId(data);
-            
+
             const stuDatanSgCg = await fetch(`/api/handleStuDataNSgSg?studentID=${data}`);
             if (!stuDatanSgCg.ok) {
                 setError(true);
                 throw new Error('Failed to fetch data');
             }
             const stuDatanSgCgRes = await stuDatanSgCg.json();
-            const {studentData, sgcgData} = stuDatanSgCgRes;
+            const { studentData, sgcgData } = stuDatanSgCgRes;
             setStudentData(studentData);
             setStudentSgCgData(sgcgData);
             const semNum = sgcgData.length;
@@ -92,88 +92,106 @@ export default function Profile() {
                             </div>
                             {studentData.length > 0 && (
                                 <div className="bg-[#18181B] m-2 p-4 rounded-lg shadow-small grid md:grid-cols-2 gap-3">
-                                <p><span className="c400 font-bold">Name -</span> {studentData[0].name}</p>
+                                    <p><span className="c400 font-bold">Name -</span> {studentData[0].name}</p>
                                     <p><span className="c400 font-bold">Roll No. -</span> {studentData[0].registrationno}</p>
                                     <p><span className="c400 font-bold">Programme -</span> {studentData[0].programme}</p>
                                     <p><span className="c400 font-bold">Branch -</span> {studentData[0].branch}</p>
                                 </div>
                             )}
 
-                            <div className="mx-2">
-                                <Divider className='mt-8 bg-sky-200 p-[1.5px] rounded-full' />
-                            </div>
-                            <div className="bg-[#18181B] m-2 p-4 rounded-lg shadow-small c400 font-bold text-2xl md:text-3xl">
-                                SGPA and CGPA
-                            </div>
-                            <div className="mx-2">
-                                <Divider className='bg-sky-200 p-[1.5px] rounded-full' />
-                            </div>
-                            {studentSgCgData.length > 0 && (
-                                <div className="my-2 px-2">
-                                    <Table isStriped className="text-center" >
-                                        <TableHeader className="p-0">
-                                            <TableColumn className="px-2 sm:px-3 text-cyan-400 font-bold text-[14px] text-center">Semester</TableColumn>
-                                            <TableColumn className="px-2 sm:px-3 text-cyan-400 font-bold text-[14px] text-center">SGPA</TableColumn>
-                                            <TableColumn className="px-2 sm:px-3 text-cyan-400 font-bold text-[14px] text-center">CGPA</TableColumn>
-                                            <TableColumn className="px-2 sm:px-3 text-cyan-400 font-bold text-[14px] text-center">Earned Credits</TableColumn>
-                                        </TableHeader>
-                                        <TableBody>
-                                            {studentSgCgData.map((item, index) => (
-                                                <TableRow key={index}>
-                                                    <TableCell>{item.stynumber}</TableCell>
-                                                    <TableCell>{item.sgpa_r}</TableCell>
-                                                    <TableCell>{item.cgpa_r}</TableCell>
-                                                    <TableCell>{item.totalearnedcredit}</TableCell>
-                                                </TableRow>
-                                            ))}
-                                        </TableBody>
-                                    </Table>
-                                </div>
-                            )}
 
 
-                            <div className="mx-2">
-                                <Divider className='mt-8 bg-sky-200 p-[1.5px] rounded-full' />
-                            </div>
-                            <div className="bg-[#18181B] m-2 p-4 rounded-lg shadow-small c400 font-bold text-2xl md:text-3xl">
-                                Subject Grades
-                            </div>
-                            <div className="mx-2">
-                                <Divider className='bg-sky-200 p-[1.5px] rounded-full' />
-                            </div>
-                            {gradesData.length > 0 && (
-                                <div className="mt-2 mb-7">
-                                    <Accordion variant="splitted">
-                                        {gradesData.map((data, index) => (
-                                            <AccordionItem key={index + 1} aria-label={`Accordion ${index + 1}`} title={`Semester ${nums[index]}`}>
-                                                <Table removeWrapper isStriped key={index + 1} className="p-0">
-                                                    <TableHeader>
-                                                        <TableColumn className="px-2 sm:px-3 text-cyan-400 font-bold text-[14px]">Subject Code</TableColumn>
-                                                        <TableColumn className="px-2 sm:px-3 text-cyan-400 font-bold text-[14px]">Subject Description</TableColumn>
-                                                        <TableColumn className="px-2 sm:px-3 text-cyan-400 font-bold text-[14px]">Grade</TableColumn>
-                                                    </TableHeader>
-                                                    <TableBody>
-                                                        {data.map((subject, idx) => (
-                                                            <TableRow key={idx}>
-                                                                <TableCell>{subject.subjectcode}</TableCell>
-                                                                <TableCell>{subject.subjectdesc}</TableCell>
-                                                                <TableCell>{subject.grade}</TableCell>
-                                                            </TableRow>
-                                                        ))}
-                                                    </TableBody>
-                                                </Table>
-                                            </AccordionItem>
-                                        ))}
-                                    </Accordion>
-                                </div>
+                            {studentSgCgData.length < 1 ? (
+                                <>
+                                    <div className="mx-2">
+                                        <Divider className='mt-8 bg-warning p-[1.5px] rounded-full' />
+                                    </div>
+                                    <div className="bg-[#18181B] m-2 p-4 rounded-lg shadow-small text-warning font-bold text-2xl md:text-3xl text-center">
+                                        No Data Sedly ;_;
+                                    </div>
+                                    <div className="mx-2">
+                                        <Divider className='bg-warning p-[1.5px] rounded-full' />
+                                    </div>
+                                </>
+                            ) : (
+                                <>
+                                    <div className="mx-2">
+                                        <Divider className='mt-8 bg-sky-200 p-[1.5px] rounded-full' />
+                                    </div>
+                                    <div className="bg-[#18181B] m-2 p-4 rounded-lg shadow-small c400 font-bold text-2xl md:text-3xl">
+                                        SGPA and CGPA
+                                    </div>
+                                    <div className="mx-2">
+                                        <Divider className='bg-sky-200 p-[1.5px] rounded-full' />
+                                    </div>
+                                    {studentSgCgData.length > 0 && (
+
+                                        <div className="my-2 px-2">
+                                            <Table isStriped className="text-center" >
+                                                <TableHeader className="p-0">
+                                                    <TableColumn className="px-2 sm:px-3 text-cyan-400 font-bold text-[14px] text-center">Semester</TableColumn>
+                                                    <TableColumn className="px-2 sm:px-3 text-cyan-400 font-bold text-[14px] text-center">SGPA</TableColumn>
+                                                    <TableColumn className="px-2 sm:px-3 text-cyan-400 font-bold text-[14px] text-center">CGPA</TableColumn>
+                                                    <TableColumn className="px-2 sm:px-3 text-cyan-400 font-bold text-[14px] text-center">Earned Credits</TableColumn>
+                                                </TableHeader>
+                                                <TableBody>
+                                                    {studentSgCgData.map((item, index) => (
+                                                        <TableRow key={index}>
+                                                            <TableCell>{item.stynumber}</TableCell>
+                                                            <TableCell>{item.sgpa_r}</TableCell>
+                                                            <TableCell>{item.cgpa_r}</TableCell>
+                                                            <TableCell>{item.totalearnedcredit}</TableCell>
+                                                        </TableRow>
+                                                    ))}
+                                                </TableBody>
+                                            </Table>
+                                        </div>
+                                    )}
+
+
+                                    <div className="mx-2">
+                                        <Divider className='mt-8 bg-sky-200 p-[1.5px] rounded-full' />
+                                    </div>
+                                    <div className="bg-[#18181B] m-2 p-4 rounded-lg shadow-small c400 font-bold text-2xl md:text-3xl">
+                                        Subject Grades
+                                    </div>
+                                    <div className="mx-2">
+                                        <Divider className='bg-sky-200 p-[1.5px] rounded-full' />
+                                    </div>
+                                    {gradesData.length > 0 && (
+                                        <div className="mt-2 mb-7">
+                                            <Accordion variant="splitted">
+                                                {gradesData.map((data, index) => (
+                                                    <AccordionItem key={index + 1} aria-label={`Accordion ${index + 1}`} title={`Semester ${nums[index]}`}>
+                                                        <Table removeWrapper isStriped key={index + 1} className="p-0">
+                                                            <TableHeader>
+                                                                <TableColumn className="px-2 sm:px-3 text-cyan-400 font-bold text-[14px]">Subject Code</TableColumn>
+                                                                <TableColumn className="px-2 sm:px-3 text-cyan-400 font-bold text-[14px]">Subject Description</TableColumn>
+                                                                <TableColumn className="px-2 sm:px-3 text-cyan-400 font-bold text-[14px]">Grade</TableColumn>
+                                                            </TableHeader>
+                                                            <TableBody>
+                                                                {data.map((subject, idx) => (
+                                                                    <TableRow key={idx}>
+                                                                        <TableCell>{subject.subjectcode}</TableCell>
+                                                                        <TableCell>{subject.subjectdesc}</TableCell>
+                                                                        <TableCell className={subject.grade === 'F' ? 'text-danger font-bold' : ''}>{subject.grade}</TableCell>
+                                                                    </TableRow>
+                                                                ))}
+                                                            </TableBody>
+                                                        </Table>
+                                                    </AccordionItem>
+                                                ))}
+                                            </Accordion>
+                                        </div>
+                                    )}
+                                </>
                             )}
+
                         </motion.div>
                     </CardBody>
 
                 </Card>
             )}
-
-            {/* <Divider/> */}
         </div>
     );
 }
